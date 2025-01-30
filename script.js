5925d84d21c35fd350c8f749df7ea215c5baab72
Gaurav Ghade <ghade.gaurav@gmail.com>
2025-01-30 14:47:32 +0530

    Cleaned the script.js

--- script.js
+++ script.js
@@ -16,10 +16,6 @@ function demoobj(name, expense1, total1) {
    this.total = total1
 }
 
-
-
- 
-
 let mainarr =[
    {
       name: "user1",
@@ -31,20 +27,6 @@ let mainarr =[
       expense: 0,
       total:0
    }
-   // {
-   //    name: "user3",
-   //    expense : 1000
-   // }
-   // ,
-   // {
-   //    name: "user4",
-   //    expense : 725
-   // }
-   // ,
-   // {
-   //    name: "user5",
-   //    expense : 1500
-   // }
 ]
 
 function calculate() {
@@ -62,23 +44,16 @@ function calculate() {
       console.log("Share is " + share)
 
       for (i = 0; i < usercount; i++) {
-         // console.log(mainarr[i].expense)
          if (mainarr[i].total > share) {
             for (j = 0; j < usercount; j++) {
-               //console.log("Second for loop")
-               if (mainarr[j].total < share) {//mainarr[i].total > share && 
-                  //console.log("Second for loop, if ")
+               if (mainarr[j].total < share) {
                   temp = mainarr[i].total - share
                   if ((mainarr[j].total + temp) > share) {
-                     //console.log(temp)
-                     //console.log("innner-if")
                      console.log(mainarr[j].name + " Gives " + (share - mainarr[j].total) + " to " + mainarr[i].name)
                      mainarr[i].total = mainarr[i].total - (share - mainarr[j].total)
                      mainarr[j].total += (share - mainarr[j].total)
                   }
                   else {
-                     //console.log(temp)
-                     //console.log("innner-else")
                      console.log(mainarr[j].name + " Gives " + (temp) + " to " + mainarr[i].name)
                      mainarr[j].total += (temp)
                      mainarr[i].total = mainarr[i].total - (temp)
@@ -90,48 +65,29 @@ function calculate() {
       isupdate = false
    }
 }
-//console.log(mainarr)
-
-//let newtempobj = new demoobj(("user"+ ++usercount),0)
-//mainarr.push(new demoobj(("user"+ usercount),0))
 
 const buttonPressed = e => {
-   //console.log(e.target.id)
    if (e.target.id.includes("user")) {
       isupdate = true
-      //console.log(e.target.id)
       tempid = e.target.id.replace("user", "")
-      //console.log(tempid)
       mainarr[tempid - 1].name = e.target.value
    }
    else if(e.target.id.includes("expense")){
       isupdate = true
-      //console.log(e.target.value)
-      //document.getElementById("heading").innerHTML+=e.target.value
-      //document.getElementById("heading2").innerHTML = parseInt(e.target.value)
       tempid = e.target.id.replace("expense", "")
       mainarr[tempid - 1].expense = parseInt(e.target.value)
    }
 }
 
-//for (let input of inputs) {
 document.addEventListener("input", e => {
-   //console.log("Hiii fuck you")
    if (e.target.matches("input"))
       buttonPressed(e);
-   })
-   //input.addEventListener("input", buttonPressed);
-//}
+})
 
 
-function myfunction() {
-   //mainarr[1].expense=100
+function addUser() {
    usercount++
-   //const newtempobj = Object.create(demoobj)
    mainarr.push(new demoobj(("user"+ usercount),0,0))
-   //mainarr.push(Object.create(demoobj))
-   //mainarr.push(newtempobj);
-   //document.getElementById("heading").innerHTML="Ok now working"
    const newuser = document.createElement("input");
    const newexpense = document.createElement("input");
    newuser.placeholder = "Type Name of User " + usercount;
@@ -142,22 +98,7 @@ function myfunction() {
    newexpense.id="expense"+usercount
    inputDiv.appendChild(newuser);
    expenseDiv.appendChild(newexpense);
-  // document.getElementById("heading2").remove()
-  // mainarr[0].name=document.getElementById("user1").value
-   //console.log(document.getElementById("user1").value)
-   //console.log(usercount)
-  // mainarr[2].name = "Gandu"
-   //console.log(mainarr[3].name)
-  // console.log(mainarr)
-
 }
 
-adduser.addEventListener("click", myfunction)
+adduser.addEventListener("click", addUser)
 submit.addEventListener("click", calculate)
-// const element = document.getElementById("adduser-btn");
-// element.addEventListener("click", myFunction);
-
-// function myFunction() {
-//    console.log("Button Clicked")
-//   document.getElementById("heading").innerHTML = "Hello World";
-// }
